8e87dd8ba146cdab5549c856a285f509e7ea74fd
mpcarolin <mpcarolin@gmail.com>
2018-11-18 09:56:06 -0700

    initial commit

---
+++ precache-manifest.24eac703e093f6d57d477d3723c94954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1075b54900d77cee262a",
+    "url": "/static/js/main.1075b549.chunk.js"
+  },
+  {
+    "revision": "51a714a613d2fc83176d",
+    "url": "/static/js/1.51a714a6.chunk.js"
+  },
+  {
+    "revision": "1075b54900d77cee262a",
+    "url": "/static/css/main.6269d259.chunk.css"
+  },
+  {
+    "revision": "12c97306fd31c094561677c93514e6f9",
+    "url": "/index.html"
+  }
+];
